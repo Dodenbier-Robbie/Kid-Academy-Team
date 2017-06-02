54ac506e1c7ddc7591153bf28214fa0974938985
rdodenbier <rdodenbier@192.168.1.17>
2017-06-01 20:32:35 -0400

    Create View Class

---
+++ src/kidacademy/control/GameControl.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kidacademy.control;
+
+import kidacademy.model.Player;
+
+/**
+ *
+ * @author rdodenbier
+ */
+public class GameControl {
+
+    public static Player createPlayer(String playersName) {
+        System.out.println("\n*** createPlayer() called ***");
+        return new Player();
+    
+}
