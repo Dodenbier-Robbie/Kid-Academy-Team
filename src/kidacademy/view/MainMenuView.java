359012d7082ecbb01307f566864f9970bab1e710
Mann <Mann@Mann-PC.attlocal.net>
2017-06-14 19:40:23 -0500

    Added View and ViewInterface, then hooked it up to MainMenuView

--- src/kidacademy/view/MainMenuView.java
+++ src/kidacademy/view/MainMenuView.java
@@ -13,64 +13,27 @@ import kidacademy.control.GameControl;
  *
  * @author Mann
  */
-public class MainMenuView {
-    private String menu;
+public class MainMenuView extends View {
     
     public MainMenuView(){
-        this.menu = "\n"
-                + "\n-----------------------------------------"
-                + "\n |Main Menu                             |"
-                + "\n-----------------------------------------"
-                + "\n1 - Go to school"
-                + "\n2 - Continue a saved game"
-                + "\n3 - Help menu"
-                + "\n4 - Tutorial"
-                + "\n5 - High score menu"
-                + "\nQ - Quit"
-                + "\n-----------------------------------------";
+        super("\n"
+            + "\n-----------------------------------------"
+            + "\n |Main Menu                             |"
+            + "\n-----------------------------------------"
+            + "\n1 - Go to school"
+            + "\n2 - Continue a saved game"
+            + "\n3 - Help menu"
+            + "\n4 - Tutorial"
+            + "\n5 - High score menu"
+            + "\nQ - Quit"
+            + "\n-----------------------------------------");
     }
     
-    void displayMainMenuView() {
- 
-            boolean done = false; // set flag to not done
-            do {
-                //prompt for and get players name
-                String menuOption = this.getMenuOption();
-                if (menuOption.toUpperCase().equals("Q")) //user wants to quit
-                    return; // exit the game
-                
-                // do the requested action and display the next view
-                done = this.doAction(menuOption);
-            
-            } while (!done);
-    }
-
-    private String getMenuOption() {
-        Scanner keyboard = new Scanner(System.in); //get infile for keyboard
-        String choice = ""; //value to be returned
-        boolean valid = false; //initalize to not valid
+    @Override
+    public boolean doAction(String value) {
+        value = value.toUpperCase(); //convert choice to upper case
         
-        while (!valid) { //loop while and invalid value is enter
-            System.out.println("\n" + this.menu); 
-            
-            choice = keyboard.nextLine(); //get next line typed on keyboard
-            choice = choice.trim(); // trim off leading and trailing breaks
-            
-            if (choice.length() < 1) { //value is blank
-                System.out.println("\nInvalid value: value cannot be blank");
-                continue;
-            }
-            
-            break; //end the loop
-        }
-        
-        return choice; //return the value entered
-    }
-
-    private boolean doAction(String choice) {
-        choice = choice.toUpperCase(); //convert choice to upper case
-        
-        switch (choice) {
+        switch (value) {
             case "1": //create and start a new game
                 this.startNewGame();
                 break;
@@ -102,7 +65,7 @@ public class MainMenuView {
         gameMenu.displayMenu();
         //***test code*** temporarily adding in link to question view
         //QuestionView questionView = new QuestionView();
-        //questionView.displayQuestionMenuView();
+        //QuestionView.displayQuestionMenuView();
     }
 
     private void startExistingGame() {
